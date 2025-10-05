f76ded6767283d24895f7ed6362268fa8666f0df
sarthakpatil7900 <sarthakpatil8281@gmail.com>
2025-10-05 19:53:41 +0530

    Update Sidebar.jsx

--- admin/src/Components/Sidebar/Sidebar.jsx
+++ admin/src/Components/Sidebar/Sidebar.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import './Sidebar.css'
 import { Link } from 'react-router-dom'
-import add_product_icon from '/src/assets/Product_cart.svg';
-import list_product_icon from '/src/assets/Product_list_icon.svg';
+const add_product_icon = new URL('../assets/Product_cart.svg', import.meta.url).href;
+const list_product_icon = new URL('../assets/Product_list_icon.svg', import.meta.url).href;
+
 
 const Sidebar = () => {
     return (
